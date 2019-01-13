8bfb7d4d65d2fe879829aa7c177ccc633ea174ef
ikechan-nichu <ikechan@ikechannoMacBook-ea.local>
2019-01-14 00:19:15 +0900

    test commit

--- GameEngine.js
+++ GameEngine.js
@@ -14,6 +14,7 @@ import "./Interface.js"
 
 // TODO: frozen sealed(overloadの禁止)
 
+
 /*こういう拡張あり？*/
 Set.prototype.filter = function(...args){ return Array.from(this).filter(...args) }
 Set.prototype.reduce = function(...args){ return Array.from(this).reduce(...args) }
