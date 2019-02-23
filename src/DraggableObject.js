9b6adb66b2a83c996790c86f2119bd77df407c1c
ikechan-nichu <ikechan@ikechannoMacBook-ea.local>
2019-02-23 21:47:44 +0900

    Origin周りの修正

--- src/DraggableObject.js
+++ src/DraggableObject.js
@@ -15,16 +15,17 @@ class Draggable extends LILLA.OBJECT.RigidObject{
             .filter(input => input.getMouse(0) && !input.getMouseDown(0))
             .map(input => input.getMousePos())
             .subscribe(pos => {
-                this.x = pos.x - offset.x
-                this.y = pos.y - offset.y
+                this.g.x = pos.x - offset.x
+                this.g.y = pos.y - offset.y
+                console.log(this.g.x, this.sprite.x)
             })
         this.InputAsObservable
             .filter(input => input.getMouseDown(0))
             .filter(input => this.hitArea.includes(input.getMousePos().x, input.getMousePos().y))
             .subscribe(input => {
                 isGrabbing = true
-                offset.x = input.getMousePos().x - this.x
-                offset.y = input.getMousePos().y - this.y
+                offset.x = input.getMousePos().x - this.g.x
+                offset.y = input.getMousePos().y - this.g.y
             })
         this.InputAsObservable
             .filter(_ => isGrabbing)
--- src/Main.js
+++ src/Main.js
@@ -2,8 +2,8 @@ import LILLA from "./LILLA.js"
 import Draggable from "./DraggableObject.js"
 
 class TextLabel extends LILLA.OBJECT.RigidObject{
-    constructor(name, tag, origin, figure, content, textColor, bgColor){
-        super(name, tag, origin, LILLA.SPRITE.Figure(figure, bgColor), figure.copy())
+    constructor(name, tag, pos, figure, content, textColor, bgColor){
+        super(name, tag, pos, LILLA.SPRITE.Figure(figure, bgColor), figure.copy())
         this.content = content
         this.textColor = textColor
     }
@@ -12,7 +12,7 @@ class TextLabel extends LILLA.OBJECT.RigidObject{
         ctx.textAlign = "left"
         ctx.textBaseline = "top"
         ctx.fillStyle = this.textColor
-        ctx.fillText(this.content, this.hitArea.x, this.hitArea.y)
+        ctx.fillText(this.content, this.g.x, this.g.y)
     }
 }
 
@@ -20,14 +20,37 @@ class Panel extends Draggable{
     constructor(...args){
         super(...args)
         this.posMode = "absolute"
-        const titleLabel = new TextLabel("title", [], [this.x, this.y], LILLA.FIGURE.Rectangle(this.hitArea.width*0.1,this.hitArea.height*0.1,this.hitArea.width*0.8,this.hitArea.height*0.8), "TITLE", "blue", "rgba(0,0,0,00)")
+    }
+    init(){
+        const titleLabel = new TextLabel("title", [], [0, 0], LILLA.FIGURE.Rectangle(this.hitArea.width*0.1,this.hitArea.height*0.1,this.hitArea.width*0.8,this.hitArea.height*0.8), "TITLE", "blue", "rgba(0,0,0,00)")
         this.add(titleLabel)
     }
 }
 
+class Obj extends LILLA.OBJECT.RigidObject{
+    constructor(...args){
+        super(...args)
+        this.velocity = {x:1,y:1}
+    }
+    update(){
+        this.x += this.velocity.x
+        this.y += this.velocity.y
+        if(this.x > 200){
+            this.velocity.x *= -1
+            this.velocity.y *= -1
+        }
+    }
+}
+class Obj2 extends LILLA.OBJECT.RigidObject{
+    constructor(...args){
+        super(...args)
+    }
+}
 
 LILLA.OBJECT.Define(Panel)
 LILLA.OBJECT.Define(TextLabel)
+LILLA.OBJECT.Define(Obj)
+LILLA.OBJECT.Define(Obj2)
 
 window.onload = ()=>{
     LILLA.ASSETS.addImages({
@@ -50,12 +73,22 @@ window.onload = ()=>{
             })
             .Spawn({
                 clsName: "Panel",
-                args: ["panel", [], [20, 20], LILLA.SPRITE.Figure(LILLA.FIGURE.Rectangle(0,0,80,50), "rgba(0,0,0,0.1)"), LILLA.FIGURE.Rectangle(0,0,80,50)],
+                args: ["panel", [], [100, 100], LILLA.SPRITE.Figure(LILLA.FIGURE.Rectangle(0,0,80,50), "rgba(0,0,0,0.1)"), LILLA.FIGURE.Rectangle(0,0,80,50)],
                 parent: "main_scene",
                 layer: "main_layer",
             })
-
-
+            .Spawn({
+                clsName: "Obj",
+                args: ["testObject1", [], [0,0], LILLA.SPRITE.Figure(LILLA.FIGURE.Rectangle(0,0,50,50), "red"), LILLA.FIGURE.Rectangle(0,0,50,50)],
+                parent: "main_scene",
+                layer: "main_layer",
+            })
+            .Spawn({
+                clsName: "Obj2",
+                args: ["testObject2", [], [100,100], LILLA.SPRITE.Figure(LILLA.FIGURE.Rectangle(0,0,50,50), "rgba(0,255,0,0.9)"), LILLA.FIGURE.Rectangle(0,0,50,50)],
+                parent: "testObject1",
+                layer: "main_layer",
+            })
         GAME.Begin()
     })
 }
